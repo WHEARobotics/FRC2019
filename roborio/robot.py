f5735c169cfc30a85494dc0303c908a6ffa38fbc
Unknown <hunter.patterson@whea.net>
2019-02-12 14:50:58 -1000

    Re-Did Joystick Layout and Formatted
    Took out everything I don't think we need. Setup everything in Teleop for the mechanisms. Left automated section blank, don't know how exactly it will work out.

--- roborio/robot.py
+++ roborio/robot.py
@@ -19,19 +19,31 @@ class MyRobot(wpilib.TimedRobot):
         """
         Button Map for Dual Joysticks
         
-        Right Joystick:
-        1. 
-        2.
-        3. 
-        4. 
-        5. 
+        (Cargo) Intake Right Joystick:
+            #User Control Buttons
+        1. L & R Gatherer in
+        2. Wrist Down
+        3. Wrist Up
+        4. Piston
 
-        Left Joystick:
-        1. 
-        2. 
-        3. 
-        4. 
-        5. 
+            #Automated Buttons
+        6. Cargo Ground
+        7. Cargo Rocket Low
+        8. Cargo C.S. (Cargo Ship Chute)
+        9. Cargo Rocket Medium 
+
+        (Hatch Panel) Outtake Left Joystick:
+            #User Control Buttons
+        1. L & R Gatherer out
+        2. Elbow Down
+        3. Elbow Up
+        4. Piston
+
+            #Automated Buttons
+        6. Multi-Low (Rocket Hatch Port, Cargo Ship Hatch Port, Transfer Station)
+        7. Hatch Panel Rocket Medium 
+        8. Starting Configuration (For End Game Climb)
+        
         """
     
         #Here is the encoder setup for the 4 motor drivetrain
@@ -53,181 +65,65 @@ class MyRobot(wpilib.TimedRobot):
         self.r_motorFront.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast) 
         self.r_motorBack.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
 
-##        self.l_motorFront.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder, 0, 0)
-##        self.r_motorFront.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder, 0, 0)
-
         self.l_motorFront.setQuadraturePosition(0, 0)
         self.r_motorFront.setQuadraturePosition(0, 0)
+
+    
+        #Here is the encoder setup for the elbow and wrist joints
+        self.elbow = ctre.wpi_talonsrx.WPI_TalonSRX(4)
+        self.elbow.setInverted(False)
         
+        self.wrist = ctre.wpi_talonsrx.WPI_TalonSRX(5)
+        self.wrist.setInverted(False)
 
+        self.elbow.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
+        self.wrist.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
 
-        #Here is the encoder setup for the left and right chute motors
-##        self.l_chute = ctre.wpi_talonsrx.WPI_TalonSRX(5)
-##        self.l_chute.setInverted(False)
-##        self.r_chute = ctre.wpi_talonsrx.WPI_TalonSRX(6)
-##        self.r_chute.setInverted(False)
-##
-##        self.l_chute.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
-##        self.r_chute.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
-##
-##        self.l_chute.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder, 0, 0)
-##        self.r_chute.configSelectedFeedbackSensor(ctre.wpi_talonsrx.WPI_TalonSRX.FeedbackDevice.QuadEncoder, 0, 0)
-##
-##        self.l_chute.setQuadraturePosition(0, 0)
-##        self.r_chute.setQuadraturePosition(0, 0)
+        self.elbow.setQuadraturePosition(0, 0)
+        self.wrist.setQuadraturePosition(0, 0)
 
         
-        #This is the setup for the drive groups and loaders
+        #This is the setup for the gatherers
+        self.l_gatherer = ctre.victorspx.WPI_VictorSPX(6)
+        self.l_intake.setInverted(False)
+        
+        self.r_gatherer = ctre.victorspx.WPI_VictorSPX(7)
+        self.r_gatherer.setInverted(False)
+
+        self.l_gatherer.setNeutralMode(ctre.ctre.victorspx.WPI_VictorSPX.NeutralMode.Coast)
+        self.r_gatherer.setNeutralMode(ctre.ctre.victorspx.WPI_VictorSPX.NeutralMode.Coast)
+
+        #Setup for pnumatics
+        self.l_piston = wpilib.Solenoid(8 , 0)
+        self.r_piston = wpilib.Solenoid(9 , 1)
+
+        #Setup for drive groups and extras               
         self.l_joy = wpilib.Joystick(0)
         self.r_joy = wpilib.Joystick(1)
-
-##        self.l_loader = wpilib.Spark(0)
-##        self.l_loader.setInverted(False)
-##        self.r_loader = wpilib.Spark(1)
-##        self.r_loader.setInverted(False)
-
-
+        
         self.left = wpilib.SpeedControllerGroup(self.l_motorFront, self.l_motorBack)
         self.right = wpilib.SpeedControllerGroup(self.r_motorFront, self.r_motorBack)
         self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
         self.counter = 0
         self.auto_loop_counter = 0
-
-        
-##        self.auto_switch0 = wpilib.DigitalInput(0)
-##        self.auto_switch1 = wpilib.DigitalInput(1)
-##        self.auto_switch2 = wpilib.DigitalInput(2)
-##        self.auto_switch3 = wpilib.DigitalInput(3)
-##        self.optical = wpilib.DigitalInput(4)
-##        self.gyro = wpilib.ADXRS450_Gyro(0)
-##        self.gyro.calibrate()
-##        self.gyro.reset()
-##        self.xbox = wpilib.XboxController(0)
-##        self.accelerometer = wpilib.BuiltInAccelerometer(wpilib.BuiltInAccelerometer.Range.k2G)
-##        self.vel = 0
-##        self.pos = 0
-##        self.grav = 9.82      
+##        self.optical = wpilib.DigitalInput(4)      
         wpilib.CameraServer.launch()
-##        IP for camera server: http://10.38.81.101:1181/
+##        IP for camera server: http://10.38.81.2:1181/
         
         
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
-        """
-        self.l_motorFront.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast) 
-        self.l_motorBack.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
-
-        self.r_motorFront.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast) 
-        self.r_motorBack.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
-
-        self.l_chute.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
-        self.r_chute.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
-
-        self.l_motorFront.setQuadraturePosition(0, 0)
-        self.r_motorFront.setQuadraturePosition(0, 0)
-
-##        self.l_chute.setQuadraturePosition(0, 0)
-##        self.r_chute.setQuadraturePosition(0, 0)
-
-        self.auto_loop_counter = 0
-
-        self.gameData = wpilib.DriverStation.getInstance().getGameSpecificMessage()
-        if not self.gameData:
-            self.gameData = 'LLL'
-            msg = 'Empty Game Specific Message,Setting It To [0]'.format(self.gameData)
-            self.logger.warn(msg)
-        ASP = self.getAutoSwitch()
-            
-##        self.start = default_timer()
-         """
         pass
+    
     
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
-        """
-        if(self.gameData[0] == 'L'):    	
-            self.AutoPL()
-            
-        else: 
-            self.AutoPR()
-            msg = 'Posistion of Auto Switch {0}'.format(self.getAutoSwitch())
-        self.logger.info(msg)
-        """
         pass
     
-    """
-    def AutoPL(self):
-        if self.getAutoSwitch() < 6:
-##            time = default_timer() - self.start
-            if self.auto_loop_counter <= 100:
-                self.drive.curvatureDrive(0.5,0,False)
-##                print ('secs: ',time)
-                print ('Pos L: ',self.l_motorFront.getQuadraturePosition())
-                print ('Pos R: ',self.r_motorFront.getQuadraturePosition())
 
-            elif self.auto_loop_counter <= 200:
-                self.drive.curvatureDrive(0.0,0,False)
-                self.l_chute.set(-0.5)
-                self.r_chute.set(-0.5)
-                self.l_loader.set(-0.5)
-                self.r_loader.set(-0.5)
-            else:
-                self.l_chute.set(0)
-                self.r_chute.set(0)
-                self.l_loader.set(0)
-                self.r_loader.set(0)
-        
-        else:
-##            time = default_timer() - self.start
-            if self.auto_loop_counter <= 80:
-                self.drive.curvatureDrive(0.5,0,False)
-##                print ('secs: ',time)
-                print ('Pos L: ',self.l_motorFront.getQuadraturePosition())
-                print ('Pos R: ',self.r_motorFront.getQuadraturePosition())
-
-            else:
-                self.drive.curvatureDrive(0.0,0,False)                
-        self.auto_loop_counter +=1
-    """
-    """    
-    def AutoPR(self):
-        if self.getAutoSwitch() > 10:
-##            time = default_timer() - self.start
-            if self.auto_loop_counter <= 100:
-                self.drive.curvatureDrive(0.5,0,False)
-##                print ('secs: ',time)
-                print ('Pos L: ',self.l_motorFront.getQuadraturePosition())
-                print ('Pos R: ',self.r_motorFront.getQuadraturePosition())
-
-            elif self.auto_loop_counter <= 200:
-                self.drive.curvatureDrive(0.0,0,False)
-                self.l_chute.set(-0.5)
-                self.r_chute.set(-0.5)
-                self.l_loader.set(-0.5)
-                self.r_loader.set(-0.5)
-            else:
-                self.l_chute.set(0)
-                self.r_chute.set(0)
-                self.l_loader.set(0)
-                self.r_loader.set(0)
-        
-        else:
-##            time = default_timer() - self.start
-            if self.auto_loop_counter <= 80:
-                self.drive.curvatureDrive(0.5,0,False)
-##                print ('secs: ',time)
-                print ('Pos L: ',self.l_motorFront.getQuadraturePosition())
-                print ('Pos R: ',self.r_motorFront.getQuadraturePosition())
-
-            else:
-                self.drive.curvatureDrive(0.0,0,False)                
-        self.auto_loop_counter +=1
-      """
-
-
-    
     def teleopInit(self):
+        """This function is run once each time the robot enters teleoperated mode."""
         
         self.l_motorFront.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast) 
         self.l_motorBack.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
@@ -235,31 +131,120 @@ class MyRobot(wpilib.TimedRobot):
         self.r_motorFront.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast) 
         self.r_motorBack.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
 
-##        self.l_chute.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
-##        self.r_chute.setNeutralMode(ctre.wpi_talonsrx.WPI_TalonSRX.NeutralMode.Coast)
-
         self.l_motorFront.setQuadraturePosition(0, 0)
         self.r_motorFront.setQuadraturePosition(0, 0)
 
-##        self.l_chute.setQuadraturePosition(0, 0)
-##        self.r_chute.setQuadraturePosition(0, 0)
+        self.l_intake.setNeutralMode(ctre.ctre.victorspx.WPI_VictorSPX.NeutralMode.Coast)
+        self.r_intake.setNeutralMode(ctre.ctre.victorspx.WPI_VictorSPX.NeutralMode.Coast)
+        
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        self.drive.tankDrive(self.l_joy.getRawAxis(1) , self.r_joy.getRawAxis(1))
-        #self.tankDrive = (self.xbox.getRawAxis(5) , self.xbox.getRawAxis(1))
 
-        #Right Joystick Intake for Loader and Chute(Ground Pickup 100%)
-##        if self.r_joy.getRawButton(1):
-##            self.l_loader.set(1) 
-##            self.l_chute.set(1)
-##            self.r_loader.set(1)
-##            self.r_chute.set(1)
-##        else:
-##            self.l_loader.set(0) 
-##            self.l_chute.set(0)
-##            self.r_loader.set(0)
-##            self.r_chute.set(0)
+        self.drive.tankDrive(self.l_joy.getRawAxis(1) , self.r_joy.getRawAxis(1))
+
+            #Right Joystick Buttons
+        
+        #L & R Gatherer Intake Right Joystick:
+        if self.r_joy.getRawButton(1):
+            self.l_gatherer.set(1) 
+            self.r_gatherer.set(1)  
+        else:
+            self.l_gatherer.set(0) 
+            self.r_gatherer.set(0)
+            
+
+        #Wrist Down Right Joystick:
+        if self.r_joy.getRawButton(2):
+            self.wrist.set(-1) 
+        else:
+            self.wrist.set(0)
+            
+
+        #Wrist Up Right Joystick:
+        if self.r_joy.getRawButton(3):
+            self.wrist.set(1) 
+        else:
+            self.wrist.set(0)
+
+
+        #Piston Toggle Right Joystick
+        if self.r_joy.getRawButton(4):
+            self.l_piston.set(True)
+            self.r_piston.set(True)
+        else:
+            self.1_piston.set(False)
+            self.r_piston.set(False)
+
+
+        #Cargo Ground Right Joystick
+        if self.r_joy.getRawButton(6):
+            pass
+
+
+        #Cargo Rocket Low Right Joystick
+        if self.r_joy.getRawButton(7):
+            pass
+
+
+        #Cargo C.S. (Cargo Ship) Right Joystick
+        if self.r_joy.getRawButton(8):
+            pass
+
+
+        #Cargo Rocket Medium Right Joystick
+        if self.r_joy.getRawButton(9):
+            pass
+
+
+        #Left Joystick Buttons
+        
+        #L & R Gatherer Outtake Left Joystick:
+        if self.l_joy.getRawButton(1):
+            self.l_gatherer.set(-1) 
+            self.r_gatherer.set(-1)  
+        else:
+            self.l_gatherer.set(0) 
+            self.r_gatherer.set(0)
+            
+
+        #Elbow Down Left Joystick:
+        if self.l_joy.getRawButton(2):
+            self.wrist.set(-1) 
+        else:
+            self.wrist.set(0)
+            
+
+        #Elbow Up Left Joystick:
+        if self.r_joy.getRawButton(3):
+            self.wrist.set(1) 
+        else:
+            self.wrist.set(0)
+
+
+        #Piston Toggle Left Joystick
+        if self.l_joy.getRawButton(4):
+            self.l_piston.set(True)
+            self.r_piston.set(True)
+        else:
+            self.1_piston.set(False)
+            self.r_piston.set(False)
+
+
+        #Multi-Low Left Joystick
+        if self.r_joy.getRawButton(6):
+            pass
+
+
+        #Hatch Panel Rocket Medium Left Joystick
+        if self.r_joy.getRawButton(7):
+            pass
+
+
+        #Starting Concfiguration Left Joystick
+        if self.r_joy.getRawButton(8):
+            pass
+            
 
         self.counter += 1
 
@@ -270,46 +255,16 @@ class MyRobot(wpilib.TimedRobot):
             msg = 'Velocity of Left & Right Drive Motors{0} {1}'.format(self.l_motorFront.getQuadratureVelocity() , self.r_motorFront.getQuadratureVelocity())
             self.logger.info(msg)
 
-##            msg = 'Posistion of Left & Right Chute Motors{0} {1}'.format(self.l_chute.getQuadraturePosition() , self.r_chute.getQuadraturePosition())
-##            self.logger.info(msg)
-##
-##            msg = 'Velocity of Left & Right Chute Motors{0} {1}'.format(self.l_chute.getQuadratureVelocity() , self.r_chute.getQuadratureVelocity())
-##            self.logger.info(msg)
+            msg = 'Posistion of Elbow & Wrist{0} {1}'.format(self.elbow.getQuadraturePosition() , self.wrist.getQuadraturePosition())
+            self.logger.info(msg)
 
-##            msg = 'Posistion of Auto Switch {0}'.format(self.getAutoSwitch())
-##            self.logger.info(msg)
+            msg = 'Velocity of Elbow & Wrist{0} {1}'.format(self.elbow.getQuadratureVelocity() , self.wrist.getQuadratureVelocity())
+            self.logger.info(msg)
 ##
 ##            msg = 'Status of Optical Interrupter {0}'.format(self.optical.get())
 ##            self.logger.info(msg)
 ##
-##            msg = 'Gyro Angle {0}'.format(self.gyro.getAngle())
-##            self.logger.info(msg)
-##
-##            msg = 'Gyro Rate {0}'.format(self.gyro.getRate())
-##            self.logger.info(msg)
-            
-##          msg = 'Acceleration of X + Y + Z Axes {0: 7.4f} {1: 7.4f} {2: 7.4f}'.format(self.accelerometer.getX() , self.accelerometer.getY() , self.accelerometer.getZ())
-##          self.logger.info(msg)
 
-
-
-##    def getAutoSwitch(self):
-##        ret_val=0
-##        if self.auto_switch0.get() == False:
-##            ret_val += 1
-##
-##        if self.auto_switch1.get() == False:
-##            ret_val += 2
-##
-##        if self.auto_switch2.get() == False:
-##            ret_val += 4
-##
-##        if self.auto_switch3.get() == False:
-##            ret_val += 8
-##
-##        return ret_val
-
-        
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
