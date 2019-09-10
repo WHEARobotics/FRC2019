3bc76881128033231b87015397653b7e6e9f58f9
Unknown <hunter.patterson@whea.net>
2019-09-09 14:21:14 -1000

    Commented out 2nd camera

--- roborio/vision.py
+++ roborio/vision.py
@@ -11,6 +11,6 @@ def main():
 
     #Note that the cameras will be displayed as "USB Camera 0" or 1 in the dashboard
     usb1 = cs.startAutomaticCapture(dev=0) 
-    usb2 = cs.startAutomaticCapture(dev=1)
+    #usb2 = cs.startAutomaticCapture(dev=1)
 
     cs.waitForever()
